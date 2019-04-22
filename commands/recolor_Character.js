c0b8ade86f719dab5f86c520665e4ab060bff05f
Ming-Yi Lu <s17113234@stu.edu.tw>
2019-04-23 03:31:08 +0800

    新增指令
    1. 新增自訂顏色指令

--- commands/recolor_Character.js
+++ commands/recolor_Character.js
@@ -2,34 +2,34 @@ const Discord = require("discord.js");
 const config = require("../config.json");
 const fs = require("fs");
 const userData = require("../players_data.json");
-//玩家更名指令
-module.exports = class rename {
+//玩家更改自訂顏色指令
+module.exports = class recolor {
     constructor() {
-        this.name = 'rename',
-            this.alias = ['更名','重新命名'],
-            this.usage = '!rename'
+        this.name = 'recolor',
+            this.alias = ['修改顏色'],
+            this.usage = '!recolor'
     }
 
     async run(bot, message, args) {
         await message.delete();
         let playerID = message.author.id;
-        console.log(`使用者(ID: ${playerID})使用「角色創建」`)
-        message.reply("開始更改角色名稱，請輸入新的「角色名稱」，輸入cancel取消.");
+        console.log(`使用者(ID: ${playerID})使用「修改顏色」`)
+        message.reply("開始更改自訂顏色，請輸入新的「顏色代碼」\n本指令使用16進位碼，請善用此網址\n https://www.ebaomonthly.com/window/photo/lesson/colorList.htm \n輸入cancel取消.");
         const filter = m => m.author.id === message.author.id;
 
         message.channel.awaitMessages(filter, {
             max: 1,
             time: 20000
         }).then(collection => {
-            let newCharacterName = collection.first().content;
+            let newColor = collection.first().content;
             if (!collection.first().content) return message.reply("取消").then(msg => {
                 msg.delete(5000)
             });
-            if (newCharacterName == "cancel") return message.reply("取消").then(msg => {
+            if (newColor == "cancel") return message.reply("取消").then(msg => {
                 msg.delete(5000)
             });
-            userData[playerID].characterName = newCharacterName;
-            message.reply(`角色更名完成，新名稱為「${newCharacterName}」創建完成`).then(msg => {msg.delete(10000)});
+            userData[playerID].color = newColor;
+            message.reply(`自訂顏色更改完成，新顏色代碼為「${newColor}」`).then(msg => {msg.delete(10000)});
         }).catch(err => {
             //console.log(err)
             return message.reply("取消").then(msg => {
